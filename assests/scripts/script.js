ff7ba979a165f32f45bae23321f5c6a6dd773ffd
Diego Jonguitud G - Macbook <djonguitud95@gmail.com>
2022-10-14 18:28:58 -0500

    chore: in index.html file change data attribute to textarea tag to tow digit numbers
    feat: Set timeColor function, depending on the hour coloe change

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,8 +1,26 @@
 let currentDay = $('#currentDay');
-
+let elTimeBlock = $('.time-block');
+let elChameleon = $('textarea');
+let currentTime = moment().format('HH');
 
 //! Setting and displaying current day
-currentDay.text((moment().format('MMMM Do YYYY'))); 
+currentDay.text(moment().format('MMMM Do YYYY'));
 
+//!Change time block color depending on the hour
+function timeColor() {
+	elChameleon.each((i) => {
+		let index = Number(elChameleon[i].dataset.time);
+		let now = Number(currentTime);
 
-
+		if (index === now) {
+			elChameleon.eq(i).addClass('present');
+			console.log('present');
+		} else if (index > now) {
+			console.log('future');
+			elChameleon.eq(i).addClass('future');
+		} else if (index < now) {
+			elChameleon.eq(i).addClass('past');
+			console.log('past');
+		}
+	});
+}
